625d4064dfca3735120726d6b51937b04a3dc2ee
Hawley Helmbrecht <hhelmbre@uw.edu>
2019-11-14 18:49:07 -0800

    PEP8

--- ifmodels/preprocess.py
+++ ifmodels/preprocess.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 
-def resize (fixed_image, scale_percent):
+
+def resize(fixed_image, scale_percent):
     """
     Function that resizes that universal atlas to be in proportion with input
     images.
@@ -9,16 +10,18 @@ def resize (fixed_image, scale_percent):
     Parameters
     ----------
     fixed_image: array
-        The atlas image of the slice that will be used for fixed registration coordinates.
-    
+        The atlas image of the slice that will be used for fixed
+        registration coordinates.
+
     scale_percent: int
-        The percentage of inflating the fixed image so that it is closer in size to moving image.
+        The percentage of inflating the fixed image so that it is closer in size
+        to moving image.
 
     Returns
     -------
     resized: array
         The resized atlas slice.
-    
+
     """
     F_im = fixed_image.astype(np.uint16)
     width = int(F_im.shape[1] * scale_percent)
